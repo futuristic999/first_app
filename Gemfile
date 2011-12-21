a922d68a4723532ba87b6521bb1b1e6f4444a0ae
futuristic999 <futuristic999@gmail.com>
2011-12-21 00:59:41 +0000

    User model with passwords, and user show page

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '3.1.3'
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
-
+gem 'gravatar_image_tag'
 
 # Gems used only for assets and not required
 # in production environments by default.
@@ -28,6 +28,7 @@ group :test do
   gem 'rspec', '2.6'
   gem 'webrat', '0.7.1'
   gem 'spork', '0.8.4'
+  gem 'factory_girl_rails', '1.4.0'
 end
 
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,16 +2,35 @@ require 'spec_helper'
 
 describe UsersController do
 
-  describe "GET 'new'" do
+  render_views
+
+  describe "GET 'show'" do
+ 
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+
     it "should be successful" do
-      get 'new'
+      get :show, :id => @user
       response.should be_success
     end
 
     it "should have the right title" do
-     get 'new'
-     response.should have_selector("title", :content => "Sign up")
+      get :show, :id => @user
+      response.should have_selector("title", :content => @user.name)
     end
+    
+    it "should include the user's name" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.name)
+    end
+    
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class => "gravatar")
+    end
+
   end
 
 end
